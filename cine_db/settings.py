3c88cee748fdcf63addd5569964b743bf9b1c56e
Maximilian <max@its-not-the.network>
2025-12-09 19:41:40 -0600

    Updated settings file.

--- cine_db/settings.py
+++ cine_db/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-&bqkkszohi(o#7gkyhdc5u)sg!u6&5@2)h4au%kwrxk_7p52##'
 env_vars = os.path.join(BASE_DIR, ".env")
 dotenv.load_dotenv(env_vars)
 
